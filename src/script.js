b13659742f907be15e136cf7bb0b07099eade32f
Jeane <jeanecardoso.qa@gmail.com>
2025-08-11 23:04:28 -0300

    WIP: Integração com o Gemini

--- src/script.js
+++ src/script.js
@@ -2,6 +2,51 @@ const form = document.getElementById('pergunta-form');
 const respostaBox = document.getElementById('resposta-box');
 const respostaInput = document.getElementById('input-api-answer');
 
+const url = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent'
+
+
+document.getElementById('btn-api-key').addEventListener('click', async () => {
+    const prompt = document.getElementById('input-api-key').value;
+    const resultadoDiv = document.getElementById('resultado');
+
+    if (!prompt) {
+        alert ('Por favor, insira a chave da API');
+        console.log("clique");
+        return;
+            
+    }
+
+    try {
+        const response = await fetch(`${url}?key=${prompt}`, { // A requisição vai para o seu próprio servidor
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'keys': ''
+            },
+            body: JSON.stringify({
+              contents: [{
+                parts: [{ text: "Test" }]
+              }]
+            })
+         
+        });
+           console.log(prompt)
+
+        const data = await response.json();
+        
+        if (data.error) {
+            resultadoDiv.textContent = `Erro: ${data.error}`;
+        } else {
+            console.log("logado")
+               console.log(prompt)
+        }
+
+    } catch (error) {
+        console.error('Erro:', error);
+        resultadoDiv.textContent = 'Erro ao se comunicar com o servidor.';
+    }
+});
+
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
 
@@ -9,13 +54,33 @@ form.addEventListener('submit', async (e) => {
   if (!pergunta) return;
 
   try {
-    // Simulação de chamada à API
-    const data = await new Promise((resolve) => {
-      setTimeout(() => resolve({ texto: `Resposta para: ${pergunta}` }), 1000);
-    });
+          const response = await fetch(url, { 
+              method: 'POST',
+              headers: {
+                  'x-goog-api-key': `${prompt}`,
+                  'Content-Type': 'application/json',
+                  'keys': ''
+              },
+              body: JSON.stringify({
+                contents: [{
+                  parts: [{ text: `${pergunta}` }]
+                }]
+              })
+           
+          });
+             console.log(prompt)
 
-    respostaInput.value = data.texto;
+    respostaInput.value = response.body.contents.parts
     respostaBox.style.display = 'block';
+
+
+  // try {
+  //   // Simulação de chamada à API
+  //   const data = await new Promise((resolve) => {
+  //     setTimeout(() => resolve({ texto: `Resposta para: ${pergunta}` }), 1000);
+  //   });
+
+
   } catch (error) {
     respostaInput.value = 'Erro ao buscar resposta';
     respostaBox.style.display = 'block';
